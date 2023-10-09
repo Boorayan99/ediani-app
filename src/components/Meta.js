27c54677ae2eee5702831a21b754f0579e87714e
Boorayan99 <brianoumo505@gmail.com>
2023-10-09 13:58:35 +0300

    Cont...

---
+++ src/components/Meta.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import { Helmet } from "react-helmet";
+
+const Meta = (props) => {
+  return (
+    <Helmet>
+      <meta charset="utf-8" />
+      <title>Our Store</title>
+    </Helmet>
+  );
+};
+
+export default Meta;
